8eaa7ad5b092cf2198041b3b191dd350edc16404
Lior Kastenbaum <liorkasti@gmail.com>
2023-02-03 10:54:12 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Orientation from 'react-native-orientation-locker';
 import {
   QueryClient,
@@ -7,22 +7,105 @@ import {
 import { Provider } from 'react-redux';
 import AppContainer from './navigation';
 import configureStore from './redux/store';
+import auth from '@react-native-firebase/auth';
+import firebase from './firebase.utils';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+// import { FIREBASE_CONFIG } from "./constants/firebase.config";
+
+// export const FIREBASE_CONFIG  = {
+//   apiKey: "AIzaSyCxmVgy6lTTHnDZfr_UAlZEhFxUW_h79oM",
+//   authDomain: "mediastack-news-kasti01.appspot.com",
+//   databaseURL: "https://mediastack-news-kasti01.appspot.com",
+//   projectId: "mediastack-news-kasti01",
+//   storageBucket: "mediastack-news-kasti01.appspot.com",
+//   messagingSenderId: "770326205412",
+//   appId: "1:770326205412:android:4c6f656cceb94074162df1"
+// };
 
 const App = () => {
-  console.log('store: ', configureStore)
-  const queryClient = new QueryClient()
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-  useEffect(() => {
-    setTimeout(() => { Orientation.lockToPortrait(); });
+  // Handle user state changes
+  // function onAuthStateChanged(user) {
+  //   setUser(user);
+  //   if (initializing) setInitializing(false);
+  // }
+
+  // firebase.initializeApp(FIREBASE_CONFIG);  // Set an initializing state whilst Firebase connects
+  
+  GoogleSignin.configure({
+    webClientId: '770326205412-qpsq599n60j6m4g7dhnmgubef0bsrbkf.apps.googleusercontent.com',
   });
 
-  return (
-    <Provider store={configureStore}>
-      <QueryClientProvider client={queryClient}>
-        <AppContainer />
-      </QueryClientProvider>
-    </Provider>
-  );
+  const onGoogleButtonPress = async () => {
+    try {
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      if (userInfo) {
+        console.log("GOOGLE USER", userInfo.user);
+      }
+
+      const users = auth().signInWithCredential(googleCredential);
+      users.then((user) => { console.log('user :>> ', user); })
+
+      return users;
+    } catch {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      } else {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+        console.error(error);
+      }
+    }
+  }
+
+// useEffect(() => {
+//   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+//   console.log('subscriber :>> ', subscriber);
+//   return subscriber; // unsubscribe on unmount
+// }, []);
+
+// if (initializing) return null;
+
+
+console.log('store: ', configureStore)
+const queryClient = new QueryClient()
+
+// useEffect(() => {
+//   setTimeout(() => { Orientation.lockToPortrait(); });
+// });
+
+
+return (
+  <Provider store={configureStore}>
+    <QueryClientProvider client={queryClient}>
+    <GoogleSigninButton
+                    onPress={() => onGoogleButtonPress()}
+                    title="Google Sign-In"
+                    // style={styles.btnSocial}
+                    color={GoogleSigninButton.Color.Dark}
+                    size={GoogleSigninButton.Size.Standard}
+                />
+                
+      <AppContainer />
+    </QueryClientProvider>
+  </Provider>
+);
 };
 
 export default App
--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, Pressable, Image } from 'react-native';
 import moment from "moment";
 import { THEME } from '../constants/theme'
@@ -13,6 +13,9 @@ const MediaCard = ({ data }) => {
   const favorites = useSelector(state => state);
   console.log('favorites :>> ', favorites);
 
+    // firebase.initializeApp(FIREBASE_CONFIG);  // Set an initializing state whilst Firebase connects
+
+
   // console.log('data :>> ', data.data);
 
   const toggleHeart = (item) => {
@@ -21,40 +24,40 @@ const MediaCard = ({ data }) => {
   }
 
   return (
-    <FlatList
-      style={styles.container}
-      data={data}
-      renderItem={({ item }) => (
-        <View>
-          <Pressable onPress={() => toggleHeart(item)} style={styles.like}>
-            <LottieView
-              // progress={progress}
-              source={require('../assets/feed_heart.json')}
-              style={styles.lottie}
-            />
-          </Pressable>
-          <Text style={styles.articleTitle}>{item.title}</Text>
-          <Text style={styles.date}>{item.category}</Text>
-          <View style={styles.newsContainer}>
-            {item.image && (
-              <Image
-                source={{ uri: item.image }}
-                style={styles.imagePoster}
-                resizeMode='cover'
+      <FlatList
+        style={styles.container}
+        data={data}
+        renderItem={({ item }) => (
+          <View>
+            <Pressable onPress={() => toggleHeart(item)} style={styles.like}>
+              <LottieView
+                // progress={progress}
+                source={require('../assets/feed_heart.json')}
+                style={styles.lottie}
               />
-            )}
-            <Text style={styles.author}>{item.author}</Text>
-            <Text style={styles.date}>
-              {moment(item.published_at).format("LLL")}
-            </Text>
-            <Text style={styles.summaryText}>{item.description}</Text>
+            </Pressable>
+            <Text style={styles.articleTitle}>{item.title}</Text>
+            <Text style={styles.date}>{item.category}</Text>
+            <View style={styles.newsContainer}>
+              {item.image && (
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.imagePoster}
+                  resizeMode='cover'
+                />
+              )}
+              <Text style={styles.author}>{item.author}</Text>
+              <Text style={styles.date}>
+                {moment(item.published_at).format("LLL")}
+              </Text>
+              <Text style={styles.summaryText}>{item.description}</Text>
+            </View>
+            <View style={styles.divider} />
+  
           </View>
-          <View style={styles.divider} />
-
-        </View>
-      )}
-      keyExtractor={(item) => item.url}
-    />
+        )}
+        keyExtractor={(item) => item.url}
+      />
   )
 }
 
